91e8c65627e1ff062207b87dbbbc9ec977482e68
Eboru <garzagonzalez_08@hotmail.com>
2023-01-22 20:38:37 -0600

    AmazonSesComplaint, AmazonSesBounce, AmazonSesDelivery

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"crypto/x509"
 	"encoding/base64"
+	"encoding/json"
 	"encoding/pem"
 	"encoding/xml"
 	"errors"
@@ -14,7 +15,103 @@ import (
 	"net/url"
 	"reflect"
 	"regexp"
+	"strings"
+	"time"
 )
+
+// https://docs.aws.amazon.com/ses/latest/dg/notification-contents.html
+type JsonDateTime time.Time
+
+func (j *JsonDateTime) UnmarshalJSON(b []byte) error {
+	s := strings.Trim(string(b), "\"")
+	t, err := time.Parse(time.RFC3339, s)
+	if err != nil {
+		return err
+	}
+	*j = JsonDateTime(t)
+	return nil
+}
+
+func (j JsonDateTime) MarshalJSON() ([]byte, error) {
+	return json.Marshal(time.Time(j))
+}
+
+type AmazonSesMail struct {
+	Timestamp        JsonDateTime           `json:"timestamp"`
+	Source           string                 `json:"source"`
+	SourceArn        string                 `json:"sourceArn"`
+	SourceIp         string                 `json:"sourceIp"`
+	CallerIdentity   string                 `json:"callerIdentity"`
+	SendingAccountId string                 `json:"sendingAccountId"`
+	MessageId        string                 `json:"messageId"`
+	Destination      []string               `json:"destination"`
+	HeadersTruncated bool                   `json:"headersTruncated"`
+	Headers          []AmazonSesMailHeaders `json:"headers"`
+	CommonHeaders    map[string]interface{} `json:"commonHeaders"`
+}
+
+type AmazonSesMailHeaders struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+type AmazonSesComplaintNotification struct {
+	NotificationType string             `json:"notificationType"`
+	Complaint        AmazonSesComplaint `json:"complaint"`
+	Mail             AmazonSesMail      `json:"mail"`
+}
+
+type AmazonSesComplaint struct {
+	ComplainedRecipients  []AmazonSesComplainedRecipient `json:"complainedRecipients"`
+	Timestamp             JsonDateTime                   `json:"timestamp"`
+	FeedbackId            string                         `json:"feedbackId"`
+	ComplaintSubType      *string                        `json:"complaintSubType"`
+	UserAgent             *string                        `json:"userAgent"`
+	ComplaintFeedbackType *string                        `json:"complaintFeedbackType"`
+	ArrivalDate           *JsonDateTime                  `json:"arrivalDate"`
+}
+
+type AmazonSesComplainedRecipient struct {
+	EmailAddress string `json:"emailAddress"`
+}
+
+type AmazonSesBounceNotification struct {
+	NotificationType string          `json:"notificationType"`
+	Bounce           AmazonSesBounce `json:"bounce"`
+	Mail             AmazonSesMail   `json:"mail"`
+}
+
+type AmazonSesBounce struct {
+	BounceType        string                      `json:"bounceType"`
+	BounceSubType     string                      `json:"bounceSubType"`
+	BouncedRecipients []AmazonSesBouncedRecipient `json:"bouncedRecipients"`
+	Timestamp         JsonDateTime                `json:"timestamp"`
+	FeedbackId        string                      `json:"feedbackId"`
+	RemoteMtaIp       *string                     `json:"remoteMtaIp"`
+	ReportingMTA      *string                     `json:"reportingMTA"`
+}
+
+type AmazonSesBouncedRecipient struct {
+	EmailAddress   string  `json:"emailAddress"`
+	Action         *string `json:"action"`
+	Status         *string `json:"status"`
+	DiagnosticCode *string `json:"diagnosticCode"`
+}
+
+type AmazonSesDeliveryNotification struct {
+	NotificationType string            `json:"notificationType"`
+	Delivery         AmazonSesDelivery `json:"delivery"`
+	Mail             AmazonSesMail     `json:"mail"`
+}
+
+type AmazonSesDelivery struct {
+	Timestamp            JsonDateTime `json:"timestamp"`
+	ProcessingTimeMillis int32        `json:"processingTimeMillis"`
+	Recipients           []string     `json:"recipients"`
+	SmtpResponse         string       `json:"smtpResponse"`
+	ReportingMTA         string       `json:"reportingMTA"`
+	RemoteMtaIp          string       `json:"remoteMtaIp"`
+}
 
 // https://github.com/robbiet480/go.sns/issues/2
 var hostPattern = regexp.MustCompile(`^sns\.[a-zA-Z0-9\-]{3,}\.amazonaws\.com(\.cn)?$`)
